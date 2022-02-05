9ca9e73e40c88370e5a86b88c3607e7a609d7ce9
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-02-05 16:02:09 -0400

    Update views.py

--- hotel/views.py
+++ hotel/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, FormView
 from .models import Room, Booking 
 from .forms import AvailableForm
+from .booking_functions.availability import check_availability
 
 # Create your views here.
 
@@ -13,3 +14,12 @@ class BookingList(ListView):
     
 class BookingView(FormView):
     form_class = AvailableForm
+    template_name = 'available_form.html'
+    
+    def form_valid(self, form):
+        data = form.cleaned_data
+        room_list = Room.objects.filter(categories=data['room_categories'])
+        available_rooms = []
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
