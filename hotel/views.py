5f63f2000b071ab69a86236bdedf6ad430a4a507
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-02-05 22:22:27 -0400

    adds
    -view room details

--- hotel/views.py
+++ hotel/views.py
@@ -6,12 +6,16 @@ from hotel.booking_functions.availability import check_availability
 
 # Create your views here.
 
-class RoomList(ListView):
+class RoomListView(ListView):
     model = Room
     
 class BookingList(ListView):
     model = Booking
     
+class RoomDetailView(View):
+    def get(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
+
 class BookingView(FormView):
     form_class = AvailableForm
     template_name = 'available_form.html'
@@ -27,7 +31,7 @@ class BookingView(FormView):
         if len(available_rooms)>0:       
             room = available_rooms[0]
             booking =Booking.objects.create(
-                # user = request.user,
+                user = self.request.user,
                 room = room,
                 check_in = data['check_in'],
                 check_out = data['check_out'],
