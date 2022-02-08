317f02fbedc5beb983c1f55f1690b35bdad98d19
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-02-07 20:45:31 -0400

    add
    -style

--- hotel/views.py
+++ hotel/views.py
@@ -77,27 +77,27 @@ class RoomDetailView(View):
         else:
             return HttpResponse('This category is not avaible')
 
-class BookingView(FormView):
-    form_class = AvailableForm
-    template_name = 'available_form.html'
+# class BookingView(FormView):
+#     form_class = AvailableForm
+#     template_name = 'available_form.html'
     
-    def form_valid(self, form):
-        data = form.cleaned_data
-        room_list = Room.objects.filter(category=data['room_categories'])
-        available_rooms = []
-        for room in room_list:
-            if check_availability(room, data['check_in'], data['check_out']):
-                available_rooms.append(room)
+#     def form_valid(self, form):
+#         data = form.cleaned_data
+#         room_list = Room.objects.filter(category=data['room_categories'])
+#         available_rooms = []
+#         for room in room_list:
+#             if check_availability(room, data['check_in'], data['check_out']):
+#                 available_rooms.append(room)
          
-        if len(available_rooms)>0:       
-            room = available_rooms[0]
-            booking =Booking.objects.create(
-                user = self.request.user,
-                room = room,
-                check_in = data['check_in'],
-                check_out = data['check_out'],
-            )
-            booking.save()
-            return HttpResponse(booking)
-        else:
-            return HttpResponse('This category is not avaible')
+#         if len(available_rooms)>0:       
+#             room = available_rooms[0]
+#             booking =Booking.objects.create(
+#                 user = self.request.user,
+#                 room = room,
+#                 check_in = data['check_in'],
+#                 check_out = data['check_out'],
+#             )
+#             booking.save()
+#             return HttpResponse(booking)
+#         else:
+#             return HttpResponse('This category is not avaible')
